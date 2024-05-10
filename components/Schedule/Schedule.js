68bbbe93fb9b526a2cabfde08bc2051e0f386880
Nishka <shrimalinishka@gmail.com>
2024-05-10 10:53:54 +0530

    Fixed Schedule

--- components/Schedule/Schedule.js
+++ components/Schedule/Schedule.js
@@ -8,6 +8,18 @@ const preahvihear = Preahvihear({
   weight: ["400"],
 });
 
+const TimelineItem = ({ title, date, description }) => {
+  return (
+    <li>
+      <div>
+        <time>{title}</time>
+        <p>{date}</p>
+        <p>{description}</p>
+      </div>
+    </li>
+  );
+};
+
 function Schedule() {
   useEffect(() => {
     // define variables
@@ -49,64 +61,37 @@ function Schedule() {
 
   return (
     <>
-      <section id="schedule" className="mt-10 bg-red-600 text-blue-600">
-        <Heading title="Our Schedule" />
-        <div className="container max-w-5xl px-4 pt-3 pb-12 mx-auto">
-          <section class="intro">
-            <div class="container"></div>
-          </section>
-          <section class="timeline">
-            <ul>
-              <li>
-                <div>
-                  <time>KodeTeaser</time>
-                  <p>Date TBD</p>
-                  <p>
-                    Teams will compete in a thrilling coding knowledge journey
-                    through an online quiz.
-                    <br />
-                    We've got treats to fuel the brains of our victorious teams!
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <time>KoderRank</time>
-                  <p>Date TBD</p>
-                  <p>
-                    Teams will tackle coding challenges using their favorite
-                    languages.
-                    <br />
-                    If your team makes the cut, you're heading to the final
-                    round!
-                  </p>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <time>Kodikas Premier League (KPL)</time>
-                  <p>Date TBD</p>
-                  <p>
-                    Teams will go head-to-head in 1 vs 1 battles, aiming to
-                    conquer Kodikas and claim their coding crowns!
-                  </p>
-                </div>
-              </li>
-            </ul>
-          </section>
-          <footer class="page-footer">
-            <span>made by </span>
-            <a href="https://georgemartsoukos.com/" target="_blank">
-              <img
-                width="24"
-                height="24"
-                src="https://assets.codepen.io/162656/george-martsoukos-small-logo.svg"
-                alt="George Martsoukos logo"
-              />
-            </a>
-          </footer>
-        </div>
-      </section>
+      <section id="schedule" class="mt-10 bg-red-600 text-blue-600">
+  <Heading title="Our Schedule" />
+  <div class="container max-w-5xl px-4 pt-3 pb-12 mx-auto timeline-container">
+    <div class="timeline">
+      <ul>
+        <li>
+          <div>
+            <h3>KodeTeaser</h3>
+            <p>Date TBD</p>
+            <p>Teams will compete in a thrilling coding knowledge journey through an online quiz. We've got treats to fuel the brains of our victorious teams!</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>KoderRank</h3>
+            <p>Date TBD</p>
+            <p>Teams will tackle coding challenges using their favorite languages. If your team makes the cut, you're heading to the final round!</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <h3>Kodikas Premier League (KPL)</h3>
+            <p>Date TBD</p>
+            <p>Teams will go head-to-head in 1 vs 1 battles, aiming to conquer Kodikas and claim their coding crowns!</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+
     </>
   );
 }
